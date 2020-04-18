9557fc4b31431bc00542d306ec5e605708540de2
HeyCamCam <ansiblenotify+gh@gmail.com>
2020-04-18 13:18:15 -0400

    configure requests for steamdb.info

--- free-ninety-nine.py
+++ free-ninety-nine.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# Python version is $PYTHON_VERSION
 
 import requests
 import os
@@ -11,6 +10,14 @@ PUSHBULLET_API_KEY = os.getenv('PUSHBULLET_API_KEY')
 
 # Configure Pushbullet
 pb = Pushbullet(PUSHBULLET_API_KEY)
-push = pb.push_note("This is the title", "This is the body")
 
-push
+# Configure requests for steamdb.info
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'}
+r = requests.get('https://steamdb.info/sales/?min_discount=95&min_rating=0', headers=headers)
+status_code = str(r.status_code)
+
+# Create Pushbullet note
+#push = pb.push_note("SteamDB", "I got status code " + status_code + " when connecting to SteamDB")
+
+# Send message
+#push
