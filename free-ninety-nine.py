c9ac35c70741226a57a57e0271bd407dc9410618
HeyCamCam <ansiblenotify+gh@gmail.com>
2020-04-18 19:29:47 -0400

    program prints list correctly to terminal but not to pushbullet

--- free-ninety-nine.py
+++ free-ninety-nine.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
+
 import os
+import re
+import requests
+from bs4 import BeautifulSoup as soup
 from pushbullet import Pushbullet
 
 
@@ -13,11 +16,19 @@ pb = Pushbullet(PUSHBULLET_API_KEY)
 
 # Configure requests for steamdb.info
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'}
-r = requests.get('https://steamdb.info/sales/?min_discount=95&min_rating=0', headers=headers)
-status_code = str(r.status_code)
+website = requests.get('https://steamdb.info/sales/?min_discount=95&min_rating=0', headers=headers)
+
+# configure beautifulsoup
+website = soup(website.text, 'html.parser')
+
+# read website using beautiful soup
+for link in website.find_all('a', {'href': re.compile(r'com\/app')}):
+  RESULTS=print(link['href'])
+
+STEAM_GAMES=RESULTS
 
 # Create Pushbullet note
-#push = pb.push_note("SteamDB", "I got status code " + status_code + " when connecting to SteamDB")
-
-# Send message
-#push
+if STEAM_GAMES == None:
+  pb.push_note("SteamDB", "There are currently 0 free Steam games")
+else:
+  pb.push_note("SteamDB", "The following games are free " + STEAM_GAMES)
